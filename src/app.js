fa138fb0c6f36b609e7cf80ecd97dc437db96fcd
Ashwin Kumar C <ashwinkumartth@gmail.com>
2025-11-03 14:16:16 +0530

    multiple routehandler

--- src/app.js
+++ src/app.js
@@ -6,46 +6,40 @@ app.get(/.*fly$/, (req, res) => {
     res.send("RCCCCCCCCCCCCCBBBBBBBBBBB")
 })
 
-app.get("/user/:userId/:name/:number", (req, res) => {
-    console.log(req.params, 'PPP');
-
-    res.send({firstname: "Jon", lastName: "Doe"})
-})
-
-// app.get("/user", (req, res) => {
-//     console.log(req.query, 'QQQ');
-
+// app.get("/user/:userId/:name/:number", (req, res) => {
+//     console.log(req.params, 'PPP');
 //     res.send({firstname: "Jon", lastName: "Doe"})
 // })
 
-app.post("/user", (req, res) => {
-    // save the data to db
-    res.send("Data saved successfully");
-    
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Data deleted successfully");
-    
-})
-
-app.use("/contact/22", (req, res) => {
-    res.send("988776525")
-})
-
-app.use("/contact", (req, res) => {
-    res.send("Thank you for contacting us")
-})
-
-
-
-app.use("/about", (req, res) => {
-    res.send("This is a service application")
-})
-
-// app.use("/", (req, res) => {
-//     res.send("Welcome to website")
+// app.get("/user", (req, res) => {
+//     console.log(req.query, 'QQQ');
+//     res.send({firstname: "Jon", lastName: "Doe"})
 // })
+
+// app.use("/uer", [rh1, rh2, rh3, rh4])
+
+app.use("/user",
+  (req, res, next) => { // Route handler
+    console.log("Handling the route User 1");
+    // res.send("1st Response");
+    // console.log("ZZZZZ");
+    next()
+  },
+  [(req, res, next) => { // 2nd Route handler
+    console.log("Handling the route User 2");
+    // res.send("2nd Response");
+    next()
+  },
+  (req, res, next) => { // 3nd Route handler
+    console.log("Handling the route User 3");
+    // res.send("3rd Response");
+    next()
+  }],
+  (req, res, next) => { // 2nd Route handler
+    console.log("Handling the route User 4");
+    res.send("4th Response");
+  }
+);
 
 app.listen(4000, () => {
     console.log("Server is Successfully running on Port 4000");
